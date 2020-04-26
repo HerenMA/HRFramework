f588b2aa12b2bcdff4118eaaa051a446a53f0177
尹建军 <yinjianjun1@herenit.com>
2020-04-26 09:06:47 +0800

    [Fix] HRFramework (1.0.5).

--- HRFramework/Classes/HRFramework.framework/Headers/HRSysAuthorityUtil.h
+++ HRFramework/Classes/HRFramework.framework/Headers/HRSysAuthorityUtil.h
@@ -21,27 +21,20 @@
  */
 + (BOOL)isForceTouchAvailable:(UITraitCollection *)traitCollection;
 
+/// 检测是否允许访问相机
+/// @param permission <#permission description#>
++ (void)isAllowedCamera:(void (^)(BOOL granted))permission;
 
-/**
- 检测是否允许访问相机
-
- @return <#return value description#>
- */
-+ (BOOL)isAllowedCamera;
-
-/**
- 检测是否允许访问手机相册
-
- @return <#return value description#>
- */
-+ (BOOL)isAllowedPhotoAlbum;
+/// 检测是否允许访问手机相册
+/// @param permission <#permission description#>
++ (void)isAllowedPhotoAlbum:(void (^)(BOOL granted))permission;
 
 /**
  检测是否允许使用麦克风
  
  @param permission <#permission description#>
  */
-+ (void)isAllowedMicophone:(void(^)(BOOL granted))permission;
++ (void)isAllowedMicophone:(void (^)(BOOL granted))permission;
 
 /**
  检测是否允许使用定位服务
